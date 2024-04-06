58e806a95830c357b1cdfb2a59aca2b08b963836
Miderson Santana <103211766+Miderson@users.noreply.github.com>
2024-04-06 12:52:05 -0300

    qwqwqwqw

--- main.js
+++ main.js
@@ -46,11 +46,11 @@ function atualizaCronometro(){
         document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
         document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
         document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg"+1).textContent = calculaTempo(tempos[1])[3];   
+        document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];   
     }
 }
 
-function comecaCronometro(){
+function comecaCronometro(){qwqwq
     atualizaCronometro();
     setInterval(atualizaCronometro,1000);
 }
