af09aceecb30d812604fca29dc2dfc6fc3c1251c
Miderson Santana <103211766+Miderson@users.noreply.github.com>
2024-04-14 19:59:24 -0300

    loop

--- main.js
+++ main.js
@@ -56,3 +56,16 @@ function comecaCronometro(){qwqwq
 }
 
 comecaCronometro();
+
+let loop1 = "";
+
+
+while (loop1 !== "Athletico Paranaense") {
+loop1 = prompt("Qual o melhor time do mundo?");
+
+  if (loop1 === "Athletico Paranaense") {
+    alert("É isso aí!");
+  } else {
+    alert(NÃO é isso. Tente novamente!);
+  }
+}
